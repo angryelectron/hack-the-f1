675e3b84c808bce62f3a4b4763602f41a527787c
abythell <abythell@ieee.org>
2013-02-14 15:56:23 -0800

    add copyright notice

--- Femulator/MapViewer/src/com/angryelectron/femulator/mapviewer/NamePanel.java
+++ Femulator/MapViewer/src/com/angryelectron/femulator/mapviewer/NamePanel.java
@@ -1,3 +1,21 @@
+/**
+ * Femulator - MIDI Mapper and F1 Emulator control for Traktor
+ * Copyright 2013, Andrew Bythell <abythell@ieee.org>
+ * http://angryelectron.com/femulator
+ *
+ * Femulator is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * Femulator is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Femulator.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.angryelectron.femulator.mapviewer;
 
 public class NamePanel extends javax.swing.JPanel {
