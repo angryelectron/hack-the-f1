117e6f0966fe460ab629e9bc055fd31a3b07de1e
abythell <abythell@ieee.org>
2013-06-18 13:36:48 -0700

    typedef structs

--- firmware/f1.h
+++ firmware/f1.h
@@ -12,9 +12,6 @@
  */
 #define F1_OUTPUT_REPORT_ID	128
 #define F1_INPUT_REPORT_ID	1
-#define F1_DFU_REPORT_ID1	240
-#define F1_DFU_REPORT_ID2	208
-#define F1_DFU_REPORT_ID3	241
 #define FEMULATOR_OUTPUT_REPORT_ID 3
 
 /*
@@ -24,9 +21,9 @@
  * Report ID 128
  * 	Size 8, Count 80 -> 7-seg displays, function keys, rgb pads, stop keys
  */
-struct {
+typedef struct {
 	uint8_t reportID;
-	uint8_t rightDigit[8];		/* Each byte in the array corresponds to segment: DP, G, C, B, A, F, E, D */
+	uint8_t rightDigit[8];		/* Each byte in the array corresponds to segment: DP; G; C; B; A; F; E; D */
 	uint8_t leftDigit[8];		/* Value = SEGMENT_ON or SEGMENT_OFF */ 
 	uint8_t browse;			/* Illuminated button state : BUTTON_LED_ON or BUTTON_LED_OFF */ 
 	uint8_t size;
@@ -36,9 +33,9 @@ struct {
 	uint8_t capture;
 	uint8_t quant;
 	uint8_t sync;
-	uint8_t pad_1[3];		/* Each byte in the array corresponds to color: Blue, Red, Green */
-	uint8_t pad_2[3];		/* Min = 0, Max = 0x7D */
-	uint8_t pad_3[3];		/* eg. White = {73, 73, 73} */
+	uint8_t pad_1[3];		/* Each byte in the array corresponds to color: Blue; Red; Green */
+	uint8_t pad_2[3];		/* Min = 0; Max = 0x7D */
+	uint8_t pad_3[3];		/* eg. White = {73; 73; 73} */
 	uint8_t pad_4[3];
 	uint8_t pad_5[3];
 	uint8_t pad_6[3];
@@ -56,7 +53,7 @@ struct {
 	uint8_t stop_3[2];		/* TODO: find out what values to use */ 
 	uint8_t stop_2[2];
 	uint8_t stop_1[2];
-} F1OutputData;
+} F1OutputData_t;
 
 /* 
  * F1 Input Data Report, ID 128.  Based on data from
@@ -69,7 +66,7 @@ struct {
  *	Size 16, Count 4 -> Analog Data?
  * 
  */
-struct
+typedef struct
 {
 	uint8_t reportID;
 	uint16_t pad_state;		/* TODO: define macros to mask pads/keys */
@@ -77,6 +74,6 @@ struct
 	uint8_t  knob_value;		/* values: 0 - 0xFF */ 
 	uint16_t  analog1[4];		/* TODO: determine the data format */ 
 	uint16_t  analog2[4];		/* TODO: determine the data format */ 
-} F1InputData;
+} F1InputData_t;
 
 #endif /* F1_H */
