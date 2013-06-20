14afcdcb08e8bd1097f605344e0e621cdf22d696
Andrew Bythell <abythell@ieee.org>
2013-06-21 09:51:55 +1300

    fix imports

--- Femulator/F1Api/src/com/angryelectron/libf1/F1PlayMode.java
+++ Femulator/F1Api/src/com/angryelectron/libf1/F1PlayMode.java
@@ -25,7 +25,6 @@ import java.util.Collection;
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiMessage;
 import javax.sound.midi.MidiUnavailableException;
-import javax.sound.midi.ShortMessage;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
